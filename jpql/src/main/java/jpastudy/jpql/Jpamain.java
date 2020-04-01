861f2a1dc873bc31e3174529fade70889b0342fa
koreanstudent <hn12344@naver.com>
2020-04-01 22:49:18 +0900

    jpql 예제

---
+++ jpql/src/main/java/jpastudy/jpql/Jpamain.java
@@ -0,0 +1,37 @@
+package jpastudy.jpql;
+ 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Jpamain {
+
+	public static void main(String[] args) {
+ 
+			//  하나만 생성해서 애플리케이션 전체에서 공유 
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+			
+			// 요청이 올때마다 사용, 쓰레드간에 공유 x
+			EntityManager em = emf.createEntityManager();
+			
+			// JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+			EntityTransaction tx = em.getTransaction();
+			tx.begin();
+			
+			try {
+	 
+				
+				
+				
+				tx.commit();
+				
+			} catch (Exception e) {
+				tx.rollback();
+			} finally {
+				em.close();
+			}
+			emf.close();
+		}
+
+}
