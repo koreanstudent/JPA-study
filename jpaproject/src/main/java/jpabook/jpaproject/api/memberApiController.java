477e66f935c2e4e5c46fdc72c66328c82bf0006a
koreanstudent <hn12344@naver.com>
2020-04-13 21:56:54 +0900

    회원 수정 API

--- jpaproject/src/main/java/jpabook/jpaproject/api/memberApiController.java
+++ jpaproject/src/main/java/jpabook/jpaproject/api/memberApiController.java
@@ -3,12 +3,15 @@ package jpabook.jpaproject.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpaproject.domain.Member;
 import jpabook.jpaproject.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -30,12 +33,34 @@ public class memberApiController {
 	/** * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다. */
 	// 엔티티와 API 스펙을 명확하게 분리가능
 	// 엔티티가 변경이 되도 API 스펙이 변경이 되지 않는 장점이 있다.
+	// 요청과 응답을 절대로 엔티티를 사용하지 않고 dto를 생성하여 사용하여야 한다.
 	@PostMapping("/api/v2/members")
 	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 		Member member = new Member();
 		member.setName(request.getName());
 		Long id = memberService.join(member);
 		return new CreateMemberResponse(id);
+	}
+	
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+			 									@RequestBody @Valid UpdateMemberRequest request) {
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+		
+	}
+	
+	@Data
+	static class UpdateMemberRequest {
+		private String name;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
 	}
 
 	@Data
